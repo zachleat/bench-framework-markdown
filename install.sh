a3f9f4c2c515e80992c91eca7ca930742e9a3873
Zach Leatherman <zachleatherman@gmail.com>
2022-08-02 10:41:13 -0500

    Extra install snuck in there

--- install.sh
+++ install.sh
@@ -13,12 +13,6 @@ rm -rf node_modules && rm -rf package-lock.json
 npm cache clean --force # For benchmarking cold npm install
 time npm install
 
-echo "[bench] Install eleventy v2"
-cd ../eleventy-v2
-rm -rf node_modules && rm -rf package-lock.json
-npm cache clean --force # For benchmarking cold npm install
-time npm install
-
 echo "[bench] Install gatsby"
 cd ../gatsby/my-gatsby-site
 rm -rf node_modules && rm -rf package-lock.json
