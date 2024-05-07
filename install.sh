99b344e06e0ee33a6dd98bbc786755c52a03fa3b
Zach Leatherman <zachleat@users.noreply.github.com>
2024-05-07 16:41:51 -0500

    Remove a few legacy

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ time npm install
 
 echo "[bench] Install eleventy"
 cd ../../eleventy
+# cd eleventy
 rm -rf node_modules && rm -rf package-lock.json
 npm cache clean --force # For benchmarking cold npm install
 time npm install
@@ -19,32 +20,8 @@ rm -rf node_modules && rm -rf package-lock.json
 npm cache clean --force # For benchmarking cold npm install
 time npm install
 
-echo "[bench] Install next"
-cd ../../nextjs/my-app
-rm -rf node_modules && rm -rf package-lock.json
-npm cache clean --force # For benchmarking cold npm install
-time npm install
-
 echo "[bench] Install next (file routing)"
 cd ../../nextjs-file-routing/my-app
-rm -rf node_modules && rm -rf package-lock.json
-npm cache clean --force # For benchmarking cold npm install
-time npm install
-
-echo "[bench] Install remix"
-cd ../../remix/my-remix-app
-rm -rf node_modules && rm -rf package-lock.json
-npm cache clean --force # For benchmarking cold npm install
-time npm install
-
-echo "[bench] Install remix (file routing)"
-cd ../../remix-file-routing/my-remix-app
-rm -rf node_modules && rm -rf package-lock.json
-npm cache clean --force # For benchmarking cold npm install
-time npm install
-
-echo "[bench] Install sveltekit (mdsvex + static adapter)"
-cd ../../sveltekit
 rm -rf node_modules && rm -rf package-lock.json
 npm cache clean --force # For benchmarking cold npm install
 time npm install
